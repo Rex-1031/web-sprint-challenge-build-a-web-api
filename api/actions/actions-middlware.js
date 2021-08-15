f884920911b99020abdfb644253fe0a169515141
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-08-15 13:53:38 -0400

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,37 @@
-// add middlewares here related to actions
+const Action = require('./actions-model')
+
+// middleware to get project id from projects database
+async function validateActionId (req, res, next) {
+    console.log("Action middleware connected")
+    try {
+        const action = await Action.get(req.params.id)
+        if (!action) {
+            res.status(404).json({message: "action not found"})
+        } else {
+            req.action = action
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+// middleware to validate required notes and description fields for actions
+function validateAction (req, res, next) {
+    const { description, notes, project_id } = req.body
+    if (!notes || !notes.trim() || !description || !description.trim() || !project_id) {
+        res.status(400).json({
+            message: "missing required field (notes or description)"
+        })
+    } else {
+        req.notes = notes.trim()
+        req.description = description.trim()
+        req.project_id = project_id
+        next()
+    }
+}
+
+module.exports = {
+    validateActionId,
+    validateAction
+}
--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,52 @@
-// add middlewares here related to projects
+const Project = require('./projects-model')
+
+// middleware to get project id from projects database
+async function validateProjectId (req, res, next) {
+    try {
+        const project = await Project.get(req.params.id)
+        if (!project) {
+            res.status(404).json({message: "project not found"})
+        } else {
+            req.project = project
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+// middleware to validate required name and description fields for projects
+function validateProject (req, res, next) {
+    const { name, description } = req.body
+    if (!name || !name.trim() || !description || !description.trim()) {
+        res.status(400).json({
+            message: "missing required field (name or description)"
+        })
+    } else {
+        req.name = name.trim()
+        req.description = description.trim()
+        next()
+    }
+}
+
+// middleware to validate required name, description, completed fields for projects
+function validateProjectWithCompleted (req, res, next) {
+    const { name, description, completed } = req.body
+    if (!name || !name.trim() || !description || !description.trim() || !completed || !completed.trim()) {
+        res.status(400).json({
+            message: "missing required field (name, description, or completed)"
+        })
+    } else {
+        req.name = name.trim()
+        req.description = description.trim()
+        req.completed = completed.trim()
+        next()
+    }
+}
+
+
+module.exports = {
+    validateProjectId,
+    validateProject,
+    validateProjectWithCompleted
+}
